9207ac763a3c4b1fec155fb56105c821fc160edf
onkarshinde1 <122793973+onkarshinde1@users.noreply.github.com>
2023-03-30 11:49:01 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the number : "))
+if a < 20 :
+ print("the number is less then 20")
+
+
+
---
+++ for_loop1.py
@@ -0,0 +1,17 @@
+import numbers
+
+
+numbers = [2,4,6,8,10]
+sq = 0
+
+print("sqaure of the given number is : ")
+for value in numbers:
+    sq = value*value
+    print(sq) 
+
+print("")
+print("cube of the given number is : ")
+for value in numbers:
+    sq= value*value*value
+    print(sq)
+
