a239a106a4df0d047a5df03476d42cad57672063
onkarshinde1 <122793973+onkarshinde1@users.noreply.github.com>
2023-04-21 16:37:08 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,6 @@
+
+d1 = {"muatble":"can change", "immutable":"cant change", "sly":"cunning", "swear":"true"}
+
+a = input("Enter the word : ")
+b = a.capitalize()
+print(b," = ", d1[b])
---
+++ list_operations.py
@@ -0,0 +1,62 @@
+import numbers
+#this is the first step 
+print("first step")
+students= ["roshan", "nutan", "mayuri" , "siddhant", "dipak"]
+print(students)
+
+#this is the second step 
+print("second step")
+numbers= [ 3,6,4,65,75]
+numbers.sort()
+print(numbers)
+
+#this is the third step 
+print("third step")
+numbers= [3, 6, 4, 65, 75]
+numbers.sort()
+numbers.reverse()
+print(numbers)
+
+#this is the fourth step 
+print("fourth step")
+numbers= [3, 6, 4, 65, 75]
+print(numbers [0:5])
+
+#this is the five step 
+print("five step")
+numbers= [3, 6, 4, 65, 75]
+print(numbers [1:5])
+
+#this is the six step 
+print("six step")
+numbers= [3, 6, 4, 65, 75]
+print(numbers [::2])
+
+
+#this is the seven step 
+print("seven step")
+numbers.append(6) #the number get last position which is given in append 
+numbers.insert(1,67) #selecting number to be printed in specific index
+numbers.remove(4) #removing  the number from the list 
+numbers.pop() #removing the last position number 
+numbers[1]= 78 # changing the value on the specific index
+numbers= [3, 6, 4, 65, 75]
+
+
+# mutable = can change (list)
+# imutable = cannot change (tuppel)
+
+tp=(3,4,5) # tp is tupple 
+print(tp) # output is shown in () this bracket 
+
+
+
+
+
+ 
+
+ 
+
+
+
+
